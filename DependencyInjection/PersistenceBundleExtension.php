cba6414a9e8c1caaa967ffbea32236dae1a83bda
Jon Cram <webignition@gmail.com>
2020-12-07 16:53:53 +0000

    Add service config

---
+++ DependencyInjection/PersistenceBundleExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace webignition\BasilWorker\PersistenceBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class PersistenceBundleExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        var_dump('loading from bundle?');
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}

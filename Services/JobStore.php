98cdaf9c4ae3b1d06c91c9d237d7a86e33b2370e
Jon Cram <webignition@gmail.com>
2020-12-07 17:16:41 +0000

    Flesh out services

--- Services/JobStore.php
+++ Services/JobStore.php
@@ -10,10 +10,26 @@ use webignition\BasilWorker\PersistenceBundle\Entity\Job;
 class JobStore
 {
     private EntityManagerInterface $entityManager;
+    private Job $job;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
+    }
+
+    public function has(): bool
+    {
+        return $this->fetch() instanceof Job;
+    }
+
+    public function get(): Job
+    {
+        $job = $this->fetch();
+        if ($job instanceof Job) {
+            $this->job = $job;
+        }
+
+        return $this->job;
     }
 
     public function store(Job $job): Job
@@ -22,5 +38,12 @@ class JobStore
         $this->entityManager->flush();
 
         return $job;
+    }
+
+    private function fetch(): ?Job
+    {
+        $job = $this->entityManager->find(Job::class, Job::ID);
+
+        return $job instanceof Job ? $job : null;
     }
 }

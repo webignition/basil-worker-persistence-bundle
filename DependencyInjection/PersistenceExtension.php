839cfbeb23e71d6eb9c42f204f9b7aac3f325ec6
Jon Cram <webignition@gmail.com>
2020-12-08 14:20:15 +0000

    Load self-test services only when testing self (#8)

--- DependencyInjection/PersistenceExtension.php
+++ DependencyInjection/PersistenceExtension.php
@@ -13,12 +13,13 @@ class PersistenceExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $environment = $container->getParameter('kernel.environment');
-
         $serviceConfigurationPaths = [
             'services.yaml',
-            'services_' . $environment . '.yaml',
         ];
+
+        if ($this->isInSelfTestEnvironment($container)) {
+            $serviceConfigurationPaths[] = 'services_test.yaml';
+        }
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
@@ -29,5 +30,17 @@ class PersistenceExtension extends Extension
                 $loader->load($serviceConfigurationPath);
             }
         }
+    }
+
+    private function isInSelfTestEnvironment(ContainerBuilder $container): bool
+    {
+        if ('test' !== $container->getParameter('kernel.environment')) {
+            return false;
+        }
+
+        $kernelProjectDirectory = realpath($container->getParameter('kernel.project_dir'));
+        $bundleDirectory = realpath(__DIR__ . '/..');
+
+        return $kernelProjectDirectory === $bundleDirectory;
     }
 }
